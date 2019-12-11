b387c5702d2c8bbf60ca810623611da166d7e693
Justin Wai <justin.wai.97@gmail.com>
2019-12-11 15:54:41 -0800

    html edit, js first touch

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const apiKey ="AIzaSyDxN4-V-gzVjgDwPc9tl7aJoRDxW2lxteE"
+const searchURL ="vision.googleapis.com"
+//searchURL="maps.googleapis.com"
+// promise would wait for first api to get results, then will allow second to build out
+
+
+function watchForm(){
+console.log('hello world')
+}
+
+console.log('Hello from out here')
+
+$(watchForm);
