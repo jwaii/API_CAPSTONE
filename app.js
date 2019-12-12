30a64c1d1f39b641397db5cd7c8862bfb2ebf6c7
Justin Wai <justin.wai.97@gmail.com>
2019-12-12 15:34:25 -0800

    watch form working, display results in grogress. New Form

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 'use strict'
 
-const apiKey ="AIzaSyDxN4-V-gzVjgDwPc9tl7aJoRDxW2lxteE"
-const searchURL ="vision.googleapis.com"
-//searchURL="maps.googleapis.com"
+const Authorization ="563492ad6f91700001000001c12e6064d8854ed3b2a57603660f2ac1"
+const searchURL ="https://www.pexels.com/v1"
 // promise would wait for first api to get results, then will allow second to build out
 
+console.log('api keys working');
+
+/*
+function formatQueryParams(params){
+    console.log('format Q params ran')
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        console.log(queryItems) //call for the keys
+        return queryItems.join('&');
+}
+*/
+
+function displayResults(responseJson){
+    console.log('display Results is running');
+    console.log(responseJson);
+    
+};
+
+/* const parkList = responseJson.data
+    for (let i=0; i< parkList.length; i++){
+        console.log(parkList[i])
+        $('.ul-results').append(`<li> <a href="${parkList[i].url}" target="_blank">${parkList[i].fullName}</a> <br> ${parkList[i].description}</li>`);
+    }*/
 
 function watchForm(){
 console.log('hello world')
+    $('form').submit(event =>{
+    event.preventDefault()
+    const query = $('#js-input-section').val();
+    console.log(query);
+    console.log('watch form working')
+});
 }
 
 console.log('Hello from out here')
